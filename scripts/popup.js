e83c67dd0000ed3678c380e215057f027e98e1a0
Maxim Lugovskoy <lugovskou.myu@yandex.ru>
2022-01-23 14:52:08 +0500

    Fix: отображение информации при открытии popup

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,6 +1,7 @@
 let profileNameEdit = document.querySelector('.profile__edit-info-button');
 let buttonClose = document.querySelector('.popup__close-button');
 let popup = document.querySelector('.popup');
+let submitBtn = document.querySelector('.popup__save-button');
 
 let formElement = document.querySelector ('.popup__form')
 let nameInput = document.querySelector ('.popup__input_name')//
@@ -8,24 +9,31 @@ let jobInput = document.querySelector ('.popup__input_desc')//
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__caption')
 //
-//function popupToggle () {
-//  popup.classList.toggle('popup_opened');
-//  if (popup.classList.contains('popup_opened')) {
-//    nameInput.value = profileName.textContent;
-//    jobInput.value = profileJob.textContent;
-//  } else {
-//    nameInput.value = '';
-//    jobInput.value = '';
-//  }}
+
 //
 
-//сохранение имени из попап
 profileNameEdit.addEventListener('click', function()  {
   popup.classList.add('popup__opened');
+
+  function popupToggle () {
+     popup.classList.toggle('popup_opened');
+  if (popup.classList.contains('popup_opened')) {
+     nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
+ } else {
+    nameInput.value = '';
+    jobInput.value = '';
+ }}
+ popupToggle ()
 });
 buttonClose.addEventListener('click', function() {
   popup.classList.remove('popup__opened');
 });
+
+submitBtn.addEventListener('click', function() {
+  popup.classList.remove('popup__opened');
+});
+
 function formSubmitHandler (evt) {
   evt.preventDefault();
 
