b6a516c3e252e4af3652c28323a5762cf5795afc
Maxim Lugovskoy <lugovskou.myu@yandex.ru>
2022-02-08 00:48:47 +0500

    reset

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,32 +1,47 @@
-let profileNameEdit = document.querySelector('.profile__edit');
-let buttonClose = document.querySelector('.popup__close');
+let profileNameEdit = document.querySelector('.profile__edit-info-button');
+let buttonClose = document.querySelector('.popup__close-button');
 let popup = document.querySelector('.popup');
-let submitBtn = document.querySelector('.popup__save');
-let formElement = document.querySelector ('.popup__form')
-let nameInput = document.querySelector ('.popup__input_name')
-let jobInput = document.querySelector ('.popup__input_desc')
-let profileName = document.querySelector('.profile__name');
-let profileJob = document.querySelector('.profile__caption');
+let submitBtn = document.querySelector('.popup__save-button');
 
-profileNameEdit.addEventListener('click', function() {
+let formElement = document.querySelector ('.popup__form')
+let nameInput = document.querySelector ('.popup__input_name')//
+let jobInput = document.querySelector ('.popup__input_desc')//
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__caption')
+//
+
+//
+
+profileNameEdit.addEventListener('click', function()  {
   popup.classList.add('popup__opened');
-  nameInput.value = profileName.textContent;
-  jobInput.value = profileJob.textContent;
+
+  function popupToggle () {
+     popup.classList.toggle('popup_opened');
+  if (popup.classList.contains('popup_opened')) {
+     nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
+ } else {
+    nameInput.value = '';
+    jobInput.value = '';
+ }}
+ popupToggle ()
+});
+buttonClose.addEventListener('click', function() {
+  popup.classList.remove('popup__opened');
 });
 
-buttonClose.addEventListener('click', function() {
+submitBtn.addEventListener('click', function() {
   popup.classList.remove('popup__opened');
 });
 
 function formSubmitHandler (evt) {
   evt.preventDefault();
+
   let nameValue = nameInput.value;
   let jobValue = jobInput.value;
   profileName.textContent = nameValue;
   profileJob.textContent = jobValue;
-  popup.classList.remove('popup__opened');
 }
-
 formElement.addEventListener('submit', formSubmitHandler);
 
 
